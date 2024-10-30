e47b67a4cf6ccc9eb87d9651261161d60da137ac
Muhammad Asif <70218962+AloneAsif@users.noreply.github.com>
2024-10-30 13:02:55 +0500

    Add files via upload

---
+++ dynamic_resume/app.js
@@ -0,0 +1,51 @@
+var updateResume = function () {
+    var _a;
+    var profile = document.getElementById("profile")
+        .value;
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var role = document.getElementById("role").value;
+    var education = document.getElementById("education").value;
+    var experience = document.getElementById("experience").value;
+    var skills = document.getElementById("skills")
+        .value;
+    var profilePicture = (_a = document.getElementById("profile_picture").files) === null || _a === void 0 ? void 0 : _a[0];
+    // display resume
+    document.getElementById("display_profile_text").textContent =
+        profile;
+    document.getElementById("display_name").textContent = name;
+    document.getElementById("display_email").textContent = email;
+    document.getElementById("display_phone").textContent = phone;
+    document.getElementById("display_education").textContent =
+        education;
+    document.getElementById("display_role").textContent = role;
+    var experienceList = document.getElementById("display_experience");
+    experienceList.innerHTML = ""; // Clear existing items
+    experience.split(",").forEach(function (item) {
+        var li = document.createElement("li");
+        li.textContent = item.trim(); // Trim whitespace
+        experienceList.appendChild(li);
+    });
+    var skillsList = document.getElementById("display_skills");
+    skillsList.innerHTML = ""; // Clear existing items
+    skills.split(",").forEach(function (skill) {
+        var li = document.createElement("li");
+        li.textContent = skill.trim();
+        skillsList.appendChild(li);
+    });
+    // Update profile picture if uploaded
+    if (profilePicture) {
+        var reader_1 = new FileReader();
+        reader_1.onload = function () {
+            document.getElementById("display_profile_picture").src = reader_1.result;
+        };
+        reader_1.readAsDataURL(profilePicture);
+    }
+};
+// Event listener for form submission
+var form = document.getElementById("resume_form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form from submitting and refreshing the page
+    updateResume();
+});
